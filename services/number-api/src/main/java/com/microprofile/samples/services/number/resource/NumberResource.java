dfa98013c28f384a1dce1e9795e890de3a0bc7b6
Roberto Cortez <radcortez@yahoo.com>
2019-05-04 19:01:22 +0100

    Apply: Added content Response for API documentation.

--- services/number-api/src/main/java/com/microprofile/samples/services/number/resource/NumberResource.java
+++ services/number-api/src/main/java/com/microprofile/samples/services/number/resource/NumberResource.java
@@ -8,6 +8,8 @@ import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Metered;
 import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
@@ -47,7 +49,14 @@ public class NumberResource {
             absolute = true)
     @Operation(description = "Generate an ISBN for a book.")
     @APIResponses({
-            @APIResponse(responseCode = "200", description = "Successful, returning the value")
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Successful, returning the value",
+                    content = @Content(
+                            schema = @Schema(
+                                    implementation = String.class
+                            )
+                    ))
     })
     @RolesAllowed("number-api")
     public Response generate() {
