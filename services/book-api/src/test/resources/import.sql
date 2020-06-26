c2b68c56bd830430925e4151d3a258e177fa7237
Roberto Cortez <radcortez@yahoo.com>
2020-06-26 21:29:44 +0100

    Update to Quarkus 1.5.2.Final.

--- services/book-api/src/test/resources/import.sql
+++ services/book-api/src/test/resources/import.sql
@@ -1,1 +1,1 @@
-insert into Book (id, author, title, year, genre, isbn) values (-1, 'Akira Torayama', 'Dragon Ball', 1984, 'Manga', null)
+insert into Book (id, author, title, year, genre, isbn) values (-1, 'Akira Torayama', 'Dragon Ball', 1984, 'Manga', null);
