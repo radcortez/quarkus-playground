b57c772b8f0a248067f6f7116f2adeb0308727dc
Roberto Cortez <radcortez@yahoo.com>
2023-05-24 09:24:32 +0100

    Enum

--- services/number-api/src/main/java/com/radcortez/quarkus/playground/number/resource/NumberResource.java
+++ services/number-api/src/main/java/com/radcortez/quarkus/playground/number/resource/NumberResource.java
@@ -4,14 +4,24 @@ import com.radcortez.quarkus.playground.number.config.NumberConfig;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class NumberResource implements NumberApi {
     @Inject
     NumberConfig numberConfig;
 
+    public enum Profile {
+        admin,
+        user
+    }
+
+    @ConfigProperty(name = "agogos.cli.profile", defaultValue = "user")
+    Profile profile;
+
     @Override
     public Response generate() {
+        System.out.println("profile = " + profile);
         return Response.ok(generateNumber()).build();
     }
 
